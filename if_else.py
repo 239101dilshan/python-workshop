5fb7ad5fe305ab757ef4b76e59f635fe8848122a
unknown <239101@TECH.WYB.AC.LK>
2025-03-26 21:30:21 +0530

    first commit

---
+++ if_else.py
@@ -0,0 +1,7 @@
+num = 10
+if(num > 5):
+    print("num is grater than 5")
+elif num > 10:
+    print("num is grater than 10")       
+else:
+    print("num is not grater than 5")    
---
+++ task3.py
@@ -0,0 +1,13 @@
+num1=int(input("Num1:"))
+num2=int(input("Num2:"))
+num3=int(input("Num3:"))
+
+if(num1<num2):
+    if(num3<num2):
+     print(num2)
+elif(num1<num3):
+   print(num3)     
+elif(num1<num3):
+   print(num3)
+else:
+    print(num3)  
