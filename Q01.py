92d1a017b8194b6a36a0591a6031be6068c1f651
239101dilshan <ashokadilshan78@gmail.com>
2025-05-28 18:01:32 +0530

    Add files via upload

---
+++ Q01.py
@@ -0,0 +1,9 @@
+total = 0
+for i in range(1,11):
+    sqr = i*i
+    if((sqr%4)==0):
+        total += sqr
+        print("Sum of odd numbers from 0 to 10:",total)
+       
+    
+    
---
+++ ex02.py
@@ -0,0 +1,3 @@
+x=[1,3,65,85,3,85,60,"ashoka","dilshan"]
+for i in x:
+    print(i)
